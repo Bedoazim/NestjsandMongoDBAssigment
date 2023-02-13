7e10fc6a366a6d068b60d50171194d6cedd246ba
Bedoazim <62192783+Bedoazim@users.noreply.github.com>
2023-02-13 02:03:05 +0200

    authentication dtos Done

---
+++ src/authentication/dto/registerInput.dto.ts
@@ -0,0 +1,26 @@
+import { IsNotEmpty ,Length, IsEmail, IsPhoneNumber,IsString, Matches } from "class-validator";
+
+export class RegisterInputDto {
+
+    @IsNotEmpty({message : "You should send username"})
+    @IsString({message : "You should send a valid format"})
+    @Length(3,255,{message: "Username can't be less than 2-letters"})
+    @Matches('^[a-zA-Z\\s]+$')
+    username:string;
+
+    @IsNotEmpty({message : "You should send email"})
+    @IsString({message : "You should send a valid format"})
+    @IsEmail()
+    email:string;
+
+    @IsNotEmpty({message : "You should send phone number"})
+    @IsString({message : "You should send a valid format"})
+    @IsPhoneNumber('EG')
+    phoneNumber:string;
+
+    @IsNotEmpty({message : "You should send password"})
+    @IsString({message : "You should send a valid format"})
+    @Length(8,255,{message: "Password can't be less than 8-digits"})
+    password:string;
+
+}
